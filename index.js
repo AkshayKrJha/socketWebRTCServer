53f0e88c33a4b3fddf927bc5dc86aa8393be6d40
Akshay Kumar Jha <akshay300899@gmail.com>
2024-06-23 21:04:31 +0530

    Socket connect, disconnect events detection, reference https://socket.io/docs/v4/tutorial/step-3

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirName = dirname(fileURLToPath(import.meta.url))
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirName,"index.html"))
 });
+
+io.on("connection", (socket) => {
+    console.log("User Connected");
+    socket.on("disconnect", ()=>{
+        console.log("User disconnected");
+    })
+})
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
