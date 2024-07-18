3961aa3d627526b97cdcabbd9e12e37d78502250
Akshay Kumar Jha <akshay300899@gmail.com>
2024-07-18 17:05:39 +0530

    Socket server connect, emit, test

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { Server } from 'socket.io';
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const __dirName = dirname(fileURLToPath(import.meta.url))
+const __dirName = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirName,"index.html"))
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirName, "index.html"));
 });
 
 io.on("connection", (socket) => {
-    console.log("User Connected");
-    socket.on("disconnect", ()=>{
-        console.log("User disconnected");
-    })
-})
+  console.log("User Connected");
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+  socket.on("some-event", (value) => {
+    console.log("Received some event:", value);
+    socket.timeout(5000).emit("test",`${value} received by server`)
+  });
+});
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
