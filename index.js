197d287d635d23321b947389925c0ed29ac35a66
Akshay Kumar Jha <akshay300899@gmail.com>
2024-08-06 12:03:54 +0530

    Middleware for socket user name verification

--- index.js
+++ index.js
@@ -15,29 +15,47 @@ const io = new Server(server, {
 });
 
 const __dirName = dirname(fileURLToPath(import.meta.url));
-let nickNames = ["Automata", "Compiler", "DBMS", "CO", "Cryptography"];
+// let nickNames = ["Automata", "Compiler", "DBMS", "CO", "Cryptography"];
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirName, "index.html"));
 });
 
 io.on("connection", (socket) => {
-  let name = nickNames.shift();
-  console.log(name, "Connected");
+  // let name = nickNames.shift();
+  // console.log(name, "Connected");
+  console.log(socket.username, "Connected");
   let appendedMessage = [];
-  socket.broadcast.emit("test", `${name} just connected`);
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      userName: socket.username,
+    });
+  }
+  console.log("Users", users);
+  socket.emit("users", users); // not working
+  // socket.broadcast.emit("test", `${name} just connected`);
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    userName: socket.username,
+  });
   socket.on("disconnect", () => {
-    console.log(name, "disconnected");
-    nickNames.push(name);
-    socket.broadcast.emit("test", `${name} just dis-connected`);
-    socket.broadcast.emit("taaip off", name);
+    // console.log(name, "disconnected");
+    // nickNames.push(name);
+    // socket.broadcast.emit("test", `${name} just dis-connected`);
+    socket.broadcast.emit("taaip off", socket.username);
+    socket.broadcast.emit("user disconnected", {
+      userID: socket.id,
+      userName: socket.username,
+    });
   });
   socket.on("typing on", () => {
-    socket.broadcast.emit("taaip on", name);
+    socket.broadcast.emit("taaip on", socket.username);
   });
   socket.on("typing off", () => {
-    socket.broadcast.emit("taaip off", name)
-  })
+    socket.broadcast.emit("taaip off", socket.username);
+  });
   socket.on("some-event", (value) => {
     appendedMessage.push(value);
     console.log("Received some event:", appendedMessage);
@@ -48,6 +66,15 @@ io.on("connection", (socket) => {
       }, "")}`
     );
   });
+});
+io.use((socket, next) => {
+  const username = socket.handshake.auth.name;
+  console.log("Socket User Name", username);
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
 });
 
 server.listen(3000, () => {
